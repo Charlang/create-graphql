58f378e87d51ae3f7840693dc0822c87a6f4bf41
Charlang <chaolunlang@163.com>
2019-11-05 14:18:05 +0800

    :art: add support for context

--- template-client-spa/src/components/utils/apolloClient.ts
+++ template-client-spa/src/components/utils/apolloClient.ts
@@ -1,12 +1,27 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from 'apollo-link-context';
 
 export const link = createHttpLink({
   uri: '/graphql/%SINGLE_SPA_APP%'
 });
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  // TODO: Change to whatever your storage for auth token
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      // TODO: change to your own format
+      authorization: `Bearer ${token}` || '',
+    },
+  };
+});
+
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link
+  link: authLink.concat(httpLink)
 });
 
 export default client;
